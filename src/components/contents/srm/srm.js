271762d90d672a1065730692854a35b0e4b1fd3b
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-04-17 15:46:24 +0530

    error corrected

--- src/components/contents/srm/srm.js
+++ src/components/contents/srm/srm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from './srm.module.css';
 import Slider from './Slider/Slider';
-import Image1 from '../../../assets/images/png.png'
 
 const srm =()=>{
 return(
