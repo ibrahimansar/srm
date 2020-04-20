b02824818152c67f96cd4329b16768bfd19ae340
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-04-21 00:28:27 +0530

    .

--- src/components/contents/contact/contact.js
+++ src/components/contents/contact/contact.js
@@ -4,9 +4,8 @@ import classes from './contact.module.css';
 const contact =()=>{
 return(
      <div className= {classes.cdiv}>
-         <p>
-
-         </p>
+              <h6> Phone Number: +91 99408 55808 </h6>
+              <h6> email: mail2mlyr@gmail.com</h6>
      </div>
 );
 }
